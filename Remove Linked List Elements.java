1f75d3378da7d4aa8ffa7df34a6c5f4cc71b2ce7
vivekkp02 <vivekpathane02@gmail.com>
2020-07-21 01:38:56 +0530

    Create Remove Linked List Elements.java

---
+++ Remove Linked List Elements.java
@@ -0,0 +1,37 @@
+/*
+Remove all elements from a linked list of integers that have value val.
+
+Example:
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {             
+        while(head!=null && head.val==val)
+            head = head.next;
+        if(head == null)
+            return null;  
+        ListNode prev = head;
+        ListNode curr = head.next;
+        while(curr != null){
+            if(curr.val == val){
+                prev.next = curr.next;
+                curr = curr.next;
+                continue;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+        return head;        
+    }
+}
