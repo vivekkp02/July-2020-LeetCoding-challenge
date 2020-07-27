b3b01a019d5f0e9d689953117cc397a2c9f91dfc
vivekkp02 <vivekpathane02@gmail.com>
2020-07-27 22:21:58 +0530

    Update Construct Binary Tree from Inorder and Postorder Traversal.java

--- Construct Binary Tree from Inorder and Postorder Traversal.java
+++ Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -36,21 +36,21 @@ class Solution {
         return buildTree(inorder, inStart, inEnd, postorder, postStart, postEnd);
     }
     public TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
-		if (inStart > inEnd || postStart > postEnd)
-		    return null;
-    i	nt rootValue = postorder[postEnd];
-		TreeNode root = new TreeNode(rootValue);
+	if (inStart > inEnd || postStart > postEnd)
+	    return null;
+    	int rootValue = postorder[postEnd];
+	TreeNode root = new TreeNode(rootValue);
         int k = 0;
-		for (int i = 0; i < inorder.length; i++) {
-			if (inorder[i] == rootValue) {
-				k = i;
-				break;
-			}
-	 	} 
-	    root.left = buildTree(inorder, inStart, k - 1, postorder, postStart, postStart + k - (inStart + 1));
-	    // Becuase k is not the length, it it need to -(inStart+1) to get the length
-	    root.right = buildTree(inorder, k + 1, inEnd, postorder, postStart + k- inStart, postEnd - 1);
-	    // postStart+k-inStart = postStart+k-(inStart+1) +1
-        return root;
+	for (int i = 0; i < inorder.length; i++) {
+		if (inorder[i] == rootValue) {
+			k = i;
+			break;
+		}
+	 } 
+	 root.left = buildTree(inorder, inStart, k - 1, postorder, postStart, postStart + k - (inStart + 1));
+	 // Becuase k is not the length, it it need to -(inStart+1) to get the length
+	 root.right = buildTree(inorder, k + 1, inEnd, postorder, postStart + k- inStart, postEnd - 1);
+	 // postStart+k-inStart = postStart+k-(inStart+1) +1
+         return root;
     }
 }
