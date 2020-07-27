2523f38a63f7126fba8db1b860ebec376f3fe43b
vivekkp02 <vivekpathane02@gmail.com>
2020-07-27 22:20:03 +0530

    Update Construct Binary Tree from Inorder and Postorder Traversal.java

--- Construct Binary Tree from Inorder and Postorder Traversal.java
+++ Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -43,10 +43,10 @@ class Solution {
         int k = 0;
 		for (int i = 0; i < inorder.length; i++) {
 			if (inorder[i] == rootValue) {
-			    k = i;
-			    break;
-		    }
-	    } 
+				k = i;
+				break;
+			}
+	 	} 
 	    root.left = buildTree(inorder, inStart, k - 1, postorder, postStart, postStart + k - (inStart + 1));
 	    // Becuase k is not the length, it it need to -(inStart+1) to get the length
 	    root.right = buildTree(inorder, k + 1, inEnd, postorder, postStart + k- inStart, postEnd - 1);
